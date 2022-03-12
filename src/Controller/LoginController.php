4e855be95066966b9f0138299a0e9614e7ba62a3
juanbep <jbecapillimue@gmail.com>
2022-03-12 16:19:39 -0500

    inicio de sesión v 1.1

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -26,4 +26,15 @@ class LoginController extends AbstractController
              'error'         => $error,
           ]);
       }
+      
+      
+      
+      /**
+     * @Route("/logout", name="app_logout", methods={"GET"})
+     */
+    public function logout(): void
+    {
+        // controller can be blank: it will never be called!
+        throw new \Exception('Don\'t forget to activate logout in security.yaml');
+    }
 }
