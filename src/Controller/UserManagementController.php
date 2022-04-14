ee227affd72ddbf8ce62d44b178e2e8bba84800d
juanbep <jbecapillimue@gmail.com>
2022-04-13 21:09:00 -0500

    v 3.8

---
+++ src/Controller/UserManagementController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserManagementController extends AbstractController
+{
+    /**
+     * @Route("/user/management", name="user_management")
+     */
+    public function index(): Response
+    {
+        
+        return $this->render('user_management/index.html.twig', [
+            'controller_name' => 'UserManagementController',
+        ]);
+    }
+}
